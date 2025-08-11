50f5d94025fa559710f91798f9f028f1f026a990
Ludwig Vynck <ludwig.vynck23@gmail.com>
2025-08-11 08:50:40 +0200

    Fix onClick type error for removeCustomImage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -693,7 +693,7 @@ export default function Home() {
                     className="w-full h-48 object-cover rounded-lg"
                   />
                   <button
-                    onClick={removeCustomImage}
+                    onClick={() => removeCustomImage()}
                     className="absolute top-2 right-2 bg-red-500 text-white rounded-full p-2 hover:bg-red-600"
                   >
                     <X className="h-4 w-4" />
